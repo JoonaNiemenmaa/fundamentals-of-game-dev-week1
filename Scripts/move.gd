0418f27ee379254f18c1d92a445130094edae548
Joona Niemenmaa <joonaoskari@hotmail.com>
2025-09-03 11:20:24 +0300

    Eka kommit

---
+++ Scripts/move.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+const MOVE_SPEED = 200
+
+func _process(delta: float) -> void:
+	var move_x = Input.get_axis("MOVE_LEFT", "MOVE_RIGHT")
+	var move_y = Input.get_axis("MOVE_UP", "MOVE_DOWN")
+	$Icon.position += Vector2(move_x, move_y).normalized() * MOVE_SPEED * delta
