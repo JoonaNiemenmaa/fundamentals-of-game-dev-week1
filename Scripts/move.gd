3d10727ed2f92924bf4effeffef2d8ca201f4247
Joona <joonaoskari@hotmail.com>
2025-09-05 11:29:04 +0300

    Added gitignore and reverted changes

--- Scripts/move.gd
+++ Scripts/move.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 const MOVE_SPEED = 200
 
-func _physics_process(delta: float) -> void:
-	var move_x = int(Input.get_axis("MOVE_LEFT", "MOVE_RIGHT"))
-	var move_y = int(Input.get_axis("MOVE_UP", "MOVE_DOWN"))
+func _process(delta: float) -> void:
+	var move_x = Input.get_axis("MOVE_LEFT", "MOVE_RIGHT")
+	var move_y = Input.get_axis("MOVE_UP", "MOVE_DOWN")
 	$Icon.position += Vector2(move_x, move_y).normalized() * MOVE_SPEED * delta
 	
